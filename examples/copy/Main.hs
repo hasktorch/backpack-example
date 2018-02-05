3f94f491f99142dde6bb724a6f4a53f14c50642d
stites <sam@stites.io>
2018-02-05 16:03:03 -0500

    simple example

--- examples/copy/Main.hs
+++ examples/copy/Main.hs
@@ -1,13 +1,16 @@
 module Main where
 
-import Torch.Core.TensorFloat as F
-import Torch.Core.TensorLong as L
+-- import Torch.Core.TensorFloat as F
+-- import Torch.Core.TensorLong as L
+import Torch.Core.Tensor as T
 import Torch.Class.Tensor.Copy as Copy
 
 main :: IO ()
 main = do
-  floatTensor <- F.new
-  longTensor <- L.new
-  newFloat <- Copy.copy floatTensor
-  newLong <- Copy.copy longTensor
+  mixinTensor <- T.new
+  newMixinTensor <- Copy.copy mixinTensor
+  -- floatTensor <- L.new
+  -- newFloat <- Copy.copy floatTensor
+  -- longTensor <- L.new
+  -- newLong <- Copy.copy longTensor
 
